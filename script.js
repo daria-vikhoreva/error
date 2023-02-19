25279b04e1b7e7485aee260f0b1b2078076b6f3f
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-02-19 21:35:29 +0400

    updated script.js

--- script.js
+++ script.js
@@ -15,8 +15,18 @@ const data = [
     }
 ]
 
-data.forEach(blockObj => {
-    const block = document.createElement(blockObj.tag);
-    block.setAttribute('id', blockObj.id);
-    document.body.append(block);
-})
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+        if (!blockObj.id) throw new SyntaxError(`В данных под номером ${i + 1} нет id`);
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    })
+} catch(e) {
+    if (e.name == 'SyntaxError') {
+        console.log(e.message);
+    } else throw e;
+}
+
+// const err = new Error('ddd');
+// console.log(err.name, err.message, err.stack);
